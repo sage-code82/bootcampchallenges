24942d2d847de07b6ec879aab1da79a31cbc345f
Stephen <sagelvl40@gmail.com>
2023-10-19 21:05:06 +0100

    show all possible ways of making the number

--- TwoSum/twosum.js
+++ TwoSum/twosum.js
@@ -7,23 +7,70 @@ const arrNum = [
 
 function stretchValue(arr, target) {
   if (typeof arr === "object") {
+    const results = [];
     for (let i = 0; i < arr.length; i++) {
-      let remainder = target - arr[i];
-
-      if (arr.includes(remainder)) {
-        if (arr[i] + remainder === target) {
-          valueInput.innerText = `${arr[i]} + ${remainder} = ${target}`;
-          return;
+      for (let j = i + 1; j < arr.length; j++) {
+        if (arr[i] + arr[j] === target) {
+          results.push(`${arr[i]} + ${arr[j]}`);
         }
       }
     }
-    valueInput.innerText = "No two numbers found that add up to the target.";
+
+    if (results.length > 0) {
+      valueInput.innerText = "Ways to make the target number:";
+      results.forEach((result) => {
+        valueInput.innerText += "\n" + result;
+      });
+    } else {
+      valueInput.innerText = "No two numbers found that add up to the target.";
+    }
   }
 }
 
 twoSumButton.addEventListener("click", () => {
   const targetInput = prompt("Pick a number between 1 and 50!");
   const targetInputNum = +targetInput;
-  console.log(typeof targetInputNum);
-  stretchValue(arrNum, targetInputNum);
+
+  if (!isNaN(targetInputNum) && targetInputNum >= 1 && targetInputNum <= 50) {
+    stretchValue(arrNum, targetInputNum);
+  } else {
+    valueInput.innerText =
+      "Invalid input. Please pick a number between 1 and 50.";
+  }
 });
+
+//     for (let i = 0; i < arr.length; i++) {
+//       let remainder = target - arr[i];
+
+//       if (arr.includes(remainder)) {
+//         if (arr[i] + remainder === target) {
+//           valueInput.innerText = `${arr[i]} + ${remainder} = ${target}`;
+//           return;
+//         }
+//       }
+//     }
+//     valueInput.innerText = "No two numbers found that add up to the target.";
+//   }
+// }
+
+// twoSumButton.addEventListener("click", () => {
+//   const targetInput = prompt("Pick a number between 1 and 50!");
+//   const targetInputNum = +targetInput;
+//   console.log(typeof targetInputNum);
+//   stretchValue(arrNum, targetInputNum);
+// });
+
+// R2 animation, random start point
+
+const viewportHeight = window.innerHeight;
+const r2d2Container = document.querySelector(".r2d2-container");
+
+function setRandomStartPosition() {
+  const randomTop = Math.floor(Math.random() * viewportHeight);
+  r2d2Container.style.top = `${randomTop}px`;
+}
+
+setRandomStartPosition();
+
+// Reset the position when the animation completes
+r2d2Container.addEventListener("animationiteration", setRandomStartPosition);
